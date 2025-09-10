9723496cdb7264de287a730b7c72de3af393da47
tanaydin <huzursuz@gmail.com>
2025-09-10 11:20:04 +0200

    Basic layout for shopping list

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <html>
 
 <body>
-    <h1>Hello World</h1>
+    <h1>Shopping List</h1>
+    <ul>
+        <li><input type="checkbox"> Milk</li>
+        <li><input type="checkbox"> Eggs</li>
+        <li><input type="checkbox"> Bread</li>
+        <li><input type="checkbox"> Butter</li>
+    </ul>
+    <input type="text" placeholder="Add Item">
+    <button>Add Item</button>
 </body>
 
 </html>
